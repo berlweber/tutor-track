18b2724f8fd1b89585fbb4e5c4c26ba74a4123a4
berl weber <153825363+berlweber@users.noreply.github.com>
2026-03-04 17:36:37 +0100

    added authentication and authorization to all routes

--- tutoring/views.py
+++ tutoring/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import  ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 
 from .models import Assignment, Student
 from .forms import SessionForm
@@ -22,34 +24,62 @@ class AssignmentList(LoginRequiredMixin, ListView):
         else:
             return Assignment.objects.filter(tutor = self.request.user)
 
-class AssignmentCreate(LoginRequiredMixin, CreateView):
+class AssignmentCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Assignment
     fields = '__all__'
+
+    def test_func(self):
+        return self.request.user.is_superuser
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
     
-class AssignmentDetail(LoginRequiredMixin, DetailView):
+class AssignmentDetail(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     model = Assignment
+
+    def test_func(self): 
+        obj = self.get_object()
+        return self.request.user.is_superuser or obj.tutor == self.request.user
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['session_form'] = SessionForm()
         return context
 
-class AssignmentUpdate(LoginRequiredMixin, UpdateView):
+class AssignmentUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Assignment
     fields = ['goal', 'hourly_rate', 'sponsor', 'start_time', 'end_time']
 
-class AssignmentDelete(LoginRequiredMixin, DeleteView):
+    def test_func(self):
+        return self.request.user.is_superuser
+
+class AssignmentDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Assignment
     success_url = reverse_lazy('assignment-list')
 
-class StudentCreate(LoginRequiredMixin, CreateView):
+    def test_func(self):
+        return self.request.user.is_superuser
+
+class StudentCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Student
     fields = '__all__'
 
+    def test_func(self):
+        return self.request.user.is_superuser
+    
+def must_be_yours(func):
+    def check_and_call(request, *args, **kwargs):
+        pk = kwargs["pk"]
+        assignment = Assignment.objects.get(pk=pk)
+        if not (assignment.tutor.id == request.user.id) or request.user.is_superuser: 
+            return HttpResponse("It is not your assignment! You are not permitted to add sessions to it!",
+                        content_type="application/json", status=403)
+        return func(request, *args, **kwargs)
+    return check_and_call
+
+@must_be_yours
+@csrf_protect
 @login_required
 def add_session(request, pk):
     form = SessionForm(request.POST)
